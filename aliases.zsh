a62e3d71daa134745e564263ceac67f5eb1b5507
Wagner Sales <salesawagner@gmail.com>
2025-06-25 17:29:50 -0300

    aliases

---
+++ aliases.zsh
@@ -0,0 +1,7 @@
+# Alias para abrir Chrome com Charles Proxy
+alias chrome-charles='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir="/tmp/chrome-charles" --proxy-server="127.0.0.1:8888" --ignore-certificate-errors --allow-insecure-localhost'
+alias reload='source ~/.zshrc'
+
+# Alias úteis
+alias ll='ls -lah'
+alias gs='git status'
